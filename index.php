9d272a409ad565522d4615c5820d7678ff876358
Sander Kroeze <sander20kroeze@gmail.com>
2019-12-02 11:11:49 +0100

    basis document neergezet

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>De Schimmel Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+<div class="container">
+    <div class="title">
+        <h1>De Schimmel</h1>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            Olaf
+            <?php include(''); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            Sander
+            <?php include(''); ?>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-12">
+            Jan ik
+            <?php include(''); ?>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
+
+<?php
